d2174cafa6320f3c5f531de2e9a5c8b75891ae59
nizz <kassonn@yahoo.com>
2017-03-05 10:30:16 -0500

    assd

--- vsScreen/player_1.gd
+++ vsScreen/player_1.gd
@@ -2,7 +2,7 @@ extends TextureFrame
 
 var ryu = preload("res://charSelect/img1full.png")
 var poe = preload("res://charSelect/img2full.png")
-#var ling = preload("res://charSelect/img3full.png")
+var ling = preload("res://charSelect/img3full.png")
 var feng = preload("res://charSelect/img4full.png")
 
 
@@ -11,7 +11,7 @@ func _ready():
 		set_texture(ryu)
 	elif Globals.get("current_state") == "Poe":
 		set_texture(poe)
-#	elif Globals.get("current_state") == "Ling":
-#		set_texture(ling)
+	elif Globals.get("current_state") == "Ling":
+		set_texture(ling)
 	elif Globals.get("current_state") == "Feng":
 		set_texture(feng)
