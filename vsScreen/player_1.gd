1388b86fcc7ba1f37b1cb744fa2f21563b052cf9
nizz <kassonn@yahoo.com>
2017-03-05 09:49:11 -0500

    stuff

--- vsScreen/player_1.gd
+++ vsScreen/player_1.gd
@@ -1,10 +1,17 @@
 extends TextureFrame
 
-var sam = preload("res://charSelect/sam2.png")
-var tanner = preload("res://charSelect/tanner2.png")
+var ryu = preload("res://charSelect/img1full.png")
+var poe = preload("res://charSelect/img2full.png")
+#var ling = preload("res://charSelect/img3full.png")
+var feng = preload("res://charSelect/img4full.png")
+
 
 func _ready():
 	if Globals.get("current_state") == "Ryu":
-		set_texture(sam)
+		set_texture(ryu)
+	elif Globals.get("current_state") == "Poe":
+		set_texture(poe)
+#	elif Globals.get("current_state") == "Ling":
+#		set_texture(ling)
 	elif Globals.get("current_state") == "Feng":
-		set_texture(tanner)
+		set_texture(feng)
